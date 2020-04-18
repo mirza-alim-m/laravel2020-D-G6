c8d7269624a5568be43d6c2e69120574e7c5db8d
heninurohmi12 <hnurohmi6@gmail.com>
2020-04-18 20:25:19 +0700

    Isi script Model Ruang

--- app/Ruang.php
+++ app/Ruang.php
@@ -7,4 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Ruang extends Model
 {
     //
+    protected $table = 'ruangs';
+    protected $fillable = [
+        'kelas'
+        ,'gedung'
+    ];
+
+    public function dosenss()
+    {
+        return $this->hasMany('App\Dosens');
+    }
 }
