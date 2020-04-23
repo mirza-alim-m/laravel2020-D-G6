1d6a80ca1b8803225775a3ff77ba3a9421c43940
Anggit Setiana <anissetiana99@gmail.com>
2020-04-23 18:22:10 +0700

    Menampilkan Relasi tabel Jam Kuliah

--- resources/views/jam/show.blade.php
+++ resources/views/jam/show.blade.php
@@ -2,7 +2,7 @@
 @section('title') Detail Ruang @endsection
 @section('content')
 
-<div class="col-md-8">
+<div class="col-md-12">
     <div class="card">
     <div class="card-body">
     <b>Dosen:</b> <br/>
@@ -22,6 +22,51 @@
     <br/>
     <b>Jam:</b> <br/>
     {{$jam_Kuliah->jam}}
+    <br><br>
+    <b>Relasi Dosen:</b> <br/>
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <td>dosen_nip</td>
+          <td>dosen_nama</td>
+          <td>mata_kuliah_id</td>
+          <td>Mata Kuliah</td>
+          <td>dosen_no_telpon</td>
+          <td>dosen_alamat</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{{$jam_Kuliah->dosens->dosen_nip}}</td>
+          <td>{{$jam_Kuliah->dosens->dosen_nama}}</td>
+          <td>{{$jam_Kuliah->dosens->mata_kuliah_id}}</td>
+          <td>{{$jam_Kuliah->dosens->matkuls->mata_kuliah}}</td>
+          <td>{{$jam_Kuliah->dosens->dosen_no_telpon}}</td>
+          <td>{{$jam_Kuliah->dosens->dosen_alamat}}</td>
+        </tr>
+
+      </tbody>
+    </table>
+    <br><br>
+    <b>Relasi Ruang:</b> <br/>
+    <table class="table table-bordered">
+      <thead>
+        <tr>
+          <td>id</td>
+          <td>kelas</td>
+          <td>gedung</td>
+        </tr>
+      </thead>
+      <tbody>
+
+        <tr>
+          <td>{{$jam_Kuliah->ruangs->id}}</td>
+          <td>{{$jam_Kuliah->ruangs->kelas}}</td>
+          <td>{{$jam_Kuliah->ruangs->gedung}}</td>
+        </tr>
+
+      </tbody>
+    </table>
     <br><br>
     <a href="{{route('jamkuliah.edit', ['jamkuliah' => $jam_Kuliah->id])}}" class="btn btn-success">Edit</a>
     <a href="{{route('jamkuliah.index')}}" class="btn btn-danger">Back</a>
