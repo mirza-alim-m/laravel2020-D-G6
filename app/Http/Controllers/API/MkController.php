39aa4019ff6821b3d6e600a1e9824d8b7feb5b46
NadiatulW <uchihanadia11@gmail.com>
2020-07-07 20:05:07 +0700

    Api

--- app/Http/Controllers/API/MkController.php
+++ app/Http/Controllers/API/MkController.php
@@ -8,9 +8,9 @@ use Illuminate\Http\Request;
 
 class MkController extends Controller
 {
-    public function_construct()
+    public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth:api');
     }
     /**
      * Display a listing of the resource.
