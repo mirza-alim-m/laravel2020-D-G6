eb148e882ffc32f89a2cd4e6390b29232c49dc1e
RezaAdityaSM <aditreza57@gmail.com>
2020-07-07 21:41:22 +0700

    menambah logout

--- resources/views/layouts/global.blade.php
+++ resources/views/layouts/global.blade.php
@@ -49,6 +49,23 @@ bootstrap.min.css')}}">
         </button>
         <input class="border-dark bg-primary-darkest form-control d-none
 d-md-block w-50 ml-3 mr-2" type="text" placeholder="Search" aria- label="Search">
+<li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
         <div class="dropdown d-none d-md-block">
             {{-- @if(\Auth::user())
             <button class="btn btn-link btn-link-primary dropdown-toggle" id="navbar-dropdown" data-toggle="dropdown">
